15b90cbfaf9d9e0f392c2764cfd4c019f7aa3e33
Rohitrwt29 <162524885+Rohitrwt29@users.noreply.github.com>
2025-05-21 07:57:26 +0530

    Add files via upload

---
+++ job-board/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import Welcome from './components/welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ job-board/src/components/welcome.jsx
@@ -0,0 +1,11 @@
+// src/components/Welcome.jsx
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to the Job Board!</h1>
+      <p>This is the beginning of your React journey.</p>
+    </div>
+  );
+}
+
+export default Welcome;
